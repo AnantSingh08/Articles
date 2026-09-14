9696e685c60e939fd58e01190b341305efc4e455
Anant Singh <anantsingh@Anants-MacBook-Pro.local>
2026-09-14 17:40:18 +0530

    Moved viewmodel creation into compose

--- app/src/main/java/com/example/articleapp/MainActivity.kt
+++ app/src/main/java/com/example/articleapp/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.ViewModelProvider
 import com.example.articleapp.ui.navigation.ArticleApp
-import com.example.articleapp.ui.screens.articles.viewmodels.ArticleViewModel
-import com.example.articleapp.ui.screens.articles.viewmodels.ArticleViewModelFactory
 import com.example.articleapp.ui.theme.ArticleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,21 +12,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val app = application as ArticleApplication
-
-        val factory = ArticleViewModelFactory(
-            app.articleRepository
-        )
-
-        val viewModel = ViewModelProvider(
-            this,
-            factory
-        )[ArticleViewModel::class.java]
-
         enableEdgeToEdge()
+
         setContent {
             ArticleAppTheme {
-                ArticleApp(viewModel)
+                ArticleApp()
             }
         }
     }
--- app/src/main/java/com/example/articleapp/ui/navigation/ArticlesNavGraph.kt
+++ app/src/main/java/com/example/articleapp/ui/navigation/ArticlesNavGraph.kt
@@ -1,24 +1,42 @@
 package com.example.articleapp.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.example.articleapp.ArticleApplication
 import com.example.articleapp.domain.models.Article
 import com.example.articleapp.ui.screens.articles.ArticleDetailsScreen
 import com.example.articleapp.ui.screens.articles.ArticlesScreen
 import com.example.articleapp.ui.screens.articles.viewmodels.ArticleViewModel
+import com.example.articleapp.ui.screens.articles.viewmodels.ArticleViewModelFactory
 
 @Composable
-fun ArticleApp(viewModel: ArticleViewModel) {
+fun ArticleApp() {
+    val context = LocalContext.current
+
+    val app = context.applicationContext as ArticleApplication
+
+    val factory = ArticleViewModelFactory(
+        app.articleRepository
+    )
+
+    val viewModel: ArticleViewModel = viewModel(
+        factory = factory
+    )
+
     val navController = rememberNavController()
     val articles = viewModel.articles.collectAsStateWithLifecycle(
-        listOf()
+        emptyList()
     ).value
-    ArticleNavHost(navController = navController, articles)
+
+    ArticleNavHost(navController = navController, articles = articles)
 }
 
 @Composable
@@ -41,19 +59,17 @@ fun ArticleNavHost(
             )
         }
 
-        composable<ArticleRoute.Details> {backStackEntry ->
+        composable<ArticleRoute.Details> { backStackEntry ->
 
             val route = backStackEntry.toRoute<ArticleRoute.Details>()
             val articleId = route.articleId
-            val article = articles.first{
+            val article = articles.first {
                 it.id == articleId
             }
             ArticleDetailsScreen(
                 article = article,
                 onBackClick = {
-                    navController.navigate(
-                        route = ArticleRoute.Articles
-                    )
+                    navController.popBackStack()
                 }
             )
         }
